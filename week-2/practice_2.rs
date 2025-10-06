c8e70b66ae8fb1647c8007416a2a33a3a4ad96f9
Moeteke Kimberly <kimberly.moeteke@pau.edu.ng>
2025-10-06 22:10:07 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!(); //prints just a
+
+	println!("Hello!");//prints
+
+	println!("format{} arguments" , "some" );//print formats some arguments
+
+	println!("my name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo" );
+}
