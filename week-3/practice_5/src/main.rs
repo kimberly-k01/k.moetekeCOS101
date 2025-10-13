db8d19e7d522db677e6b382c3ee79ea25486ef67
Moeteke Kimberly <kimberly.moeteke@pau.edu.ng>
2025-10-13 23:08:22 +0100

    (My week 3 practice stuff)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@';
+    let alphabet:char = 'B';
+    let surname = "Bolaji";
+    let firstname = "Michael";
+    let middlename = "Gboyega";
+
+    println!(" ");
+    println!("Special character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}", surname, middlename, firstname);
+}
---
+++ week-3/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\ufuom\Documents\k.moetekeCOS101\week-3\practice_6\target\debug\deps\practice_6.d: src\main.rs
+
+C:\Users\ufuom\Documents\k.moetekeCOS101\week-3\practice_6\target\debug\deps\practice_6.exe: src\main.rs
+
+src\main.rs:
